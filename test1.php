30f229e7facb3f764b98c64133462aaf2250e658
Kstyle0710 <jongbaekim0710@gmail.com>
2020-07-30 07:35:47 +0900

    to learn PHP

---
+++ test1.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<body>
+
+  <h1>Data Type</h1>
+  <?php
+  echo 100/50;
+  echo "hello world"
+   ?>
+
+  <?php
+  echo 100/50;
+  echo "hello world"
+  ?>
+
+  <?php
+  echo "hello"."world"
+  ?>
+
+  <?php
+  echo strlen("hello"."world")  // 문자개수
+  ?>
+
+  <?php
+  echo strlen("hello world")
+  ?>
+
+</body>
+</html>
