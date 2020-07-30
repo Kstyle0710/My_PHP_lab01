df21ac98eb650764bdcef78b37b80d150a165d84
Kstyle0710 <jongbaekim0710@gmail.com>
2020-07-30 22:28:17 +0900

    2nd

--- index.php
+++ index.php
@@ -6,22 +6,52 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>WEB</h1>
-    <ol>
+    <h1><a href="index.php">WEB</a></h1>
+    <!-- 반복문 사용하기 전 Data 목록 불러오기 -->
+    <!-- <ol>
         <li><a href="index.php?id=HTML">HTML</a></li>
         <li><a href="index.php?id=CSS">CSS</a></li>
         <li><a href="index.php?id=JavaScript">JavaScript</a></li>
-    </ol>
+        <li><a href="index.php?id=PHP">PHP</a></li>
+    </ol> -->
+    <!-- 반복문 사용하기 -->
+    <?php
+     $list = scandir('./data');
+     $i = 0;
+     while($i<count($list)){
+       if($list[$i] != '.'){
+         if($list[$i] != '..'){
+           ?>
+           <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
+           <?php
+         }
+       }
+       $i = $i +1;
+     }
+     ?>
+
     <h2>
       <?php
-      echo $_GET['id'];
+      if(isset($_GET['id'])){  // isset(~) 괄호안에 값이 있으면 참
+        echo $_GET['id'];
+      } else {
+        echo "Welcome";
+      }
+
       ?>
     </h2>
 
     <?php
-    echo file_get_contents("data/".$_GET['id']);
+    if(isset($_GET['id'])){
+      echo file_get_contents("data/".$_GET['id']);
+    }else{
+      echo "Hello PHP";
+
+    }
 
     ?>
+<!-- id값 유무에 따른 조건문 설정 -->
+
 
 </body>
 </html>
